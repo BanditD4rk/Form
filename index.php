d676f501c00b0b8a4462dab28ce7a827e8168550
Ba1881 <Ba1881@epbjcbarreiro.pt>
2018-10-12 12:01:57 +0100

    Commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html>
+    <head>
+        <?php
+            include("head.php");
+        ?>
+    </head>
+    <body>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <?php
+                    include("Form.html");
+                    ?>
+                </div>
+            </div>
+        </div>
+        <?php
+            include("importScripts.php");
+        ?>
+    </body>
+</html>
